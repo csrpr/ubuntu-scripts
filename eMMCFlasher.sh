1b741c01b4a8ab8e7a88fe376fc885667d8a3755
Ravikumar Ronad <151502810+csrpr@users.noreply.github.com>
2024-09-09 16:18:14 +0530

    Update eMMCFlasher.sh
    use the sfdisk command

--- eMMCFlasher.sh
+++ eMMCFlasher.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Define the block device name and path 
+# Define the block device name & path 
 BLOCK=mmcblk1
 DRIVE="/dev/$BLOCK"
 
@@ -24,7 +24,6 @@ check_fdisk_version() {
 # Check fdisk version
 check_fdisk_version
 
-
 echo "[Umounting all existing partition on $DRIVE...]"
 
 # Umount any partions on the device & supress error messages
@@ -33,31 +32,9 @@ umount "/media/$BLOCK"p* &> /dev/null
 
 echo "[Creating Partition on $DRIVE...]"
 
-# Get the size of the disk in bytes
-SIZE=`fdisk -l $DRIVE | grep Disk | awk '{print $5}'`
-
-echo DISK SIZE - $SIZE bytes
-
-# Calculate the number of cylinders
-CYLINDERS=`echo $SIZE/255/63/512 | bc`
-
-echo CYLINDERS - $CYLINDERS
-
-# Create a new partition table and a single partition that spans the entire disk
-# 'o' creates a new empty DOS partition table
-# 'n' creates a new partition
-# 'p' specifies a primary partition
-# The start sector is left empty, defaulting to the beginning of the disk
-# '$SIZE' specifies the end sector, using the entire available space
-# 'w' writes the partition table to disk and exits
-echo "o
-n
-p
-1
-
-$SIZE
-w
-" | fdisk $DRIVE &> /dev/null
+sudo sfdisk ${DRIVE} <<-__EOF__
+1M,,L,*
+__EOF__
 
 sleep 1 # Wait for the partition table to be updated 
 
@@ -71,12 +48,11 @@ echo "[Making filesystem...]"
 # Umount any remaining partitons and suppress error messages
 umount "/dev/$BLOCK"p* &> /dev/null
 sleep 1
+
 umount " /media/$BLOCK"p* &> /dev/null
 sleep 2
 
-# Format the single partition with ext4 filesystem
-# 'mkfs.ext4' creates an ext4 filesystem
-# '-L rootfs' labels the filesystem as 'rootfs'
+# Format the single partition with ext4 filesystem 'mkfs.ext4' creates an ext4 filesystem, '-L rootfs' labels the filesystem as 'rootfs'
 mkfs.ext4 -L rootfs /dev/${BLOCK}p1
 
 echo "[Mounting Root Partition..]"
@@ -86,17 +62,17 @@ mount "$DRIVE"p1 /mnt
 echo "[Extracting Filesystem..]"
 # Create a mount point for the source partition 
 mkdir -p /media/mmcblk0p1
+
 # Mount the source partition to /media/mmcblk0p1
-mount /dev/mmcblk0p1 /meaid/mmcblk0p1 &> /dev/null
+mount /dev/mmcblk0p1 /media/mmcblk0p1 &> /dev/null
+
 # Copy all files from the source partition to the new partition
 cp -rf /media/mmcblk0p1/* /mnt
 
 echo "[Syncing..]"
-# Ensure all file system changes are written to disk
 sync
 
 echo "[Unmounting Root Partition]"
-# Unmount the new partition from /mnt
 umount "$DRIVE"p1
 
 # Set the environment variable to specify boot targets
